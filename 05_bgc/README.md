16f3db1a787eb807322ff1ab13ec7c2974769420
Steve Mussmann <17727337+stevemussmann@users.noreply.github.com>
2026-04-24 15:33:17 -0700

    update readme and clinehelpr scripts

--- 05_bgc/README.md
+++ 05_bgc/README.md
@@ -3,6 +3,11 @@
 These folders contain the commands and data for running the genomic cline analysis in bgc.
 
 Run order for the folders:
-1. vcf2bgc
-2. bgc
+1. filtering
+2. bgc run folders (in any order)
+    1. bgc_lowerRecom
+    2. bgc_meanRecom
+    3. bgc_upperRecom
 3. r_scripts_for_output
+    1. clinehelpr
+    2. ideogram
--- 05_bgc/r_scripts_for_output/clinehelpr/clinehelpr_lowerCI.R
+++ 05_bgc/r_scripts_for_output/clinehelpr/clinehelpr_lowerCI.R
@@ -1,14 +1,19 @@
 library("ClineHelpR")
 
+# set working directory in Rstudio
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+load("clinehelpr_lowerCI.RData")
+
 prefix="dx2003"
 plotDIR="./plots"
 
-# make sure lnl files are changed to LnL (case sensitive) before running command
-bgc.genes <- combine_bgc_output(results.dir = "./", prefix = prefix)
+# make sure lnl files are changed to LNL (case sensitive) before running command
+#bgc.genes <- combine_bgc_output(results.dir = "./", prefix = prefix)
 
 # Plot the likelihood and parameter traces
 # showPLOTS=TRUE prints the plots to the screen
-plot_traces(df.list = bgc.genes, prefix = prefix, plotDIR = plotDIR, showPLOTS=TRUE)
+plot_traces(df.list = bgc.genes, prefix = prefix, plotDIR = plotDIR, showPLOTS=FALSE)
 
 gene.outliers <-
   get_bgc_outliers(
@@ -17,7 +22,7 @@ gene.outliers <-
     popmap = "dx2003_bgc_map.txt", # popmap file path
     loci.file = "dx2003_loci.txt", # optional; loci file path; gets generated in vcf2bgc.py; optional (but required for ideograms)
     qn = 0.975)
-write.table(gene.outliers[[1]], file="dx2003_rerun.txt", sep="\t", quote=FALSE)
+write.table(gene.outliers[[1]], file="dx2003_lowerCI.txt", sep="\t", quote=FALSE)
 
 ab.range <-
   data.frame(
@@ -43,11 +48,11 @@ phiPlot(
   saveToFile = paste0(prefix, "_genes"),
   plotDIR = plotDIR,
   showPLOTS = TRUE,
-  both.outlier.tests = FALSE,
-  neutral.color = "gray60",
-  alpha.color = "#33a02c",
-  beta.color = "#ff7f00",
-  both.color = "#6a3d9a",
+  both.outlier.tests = TRUE,
+  neutral.color = "gray90",
+  alpha.color = "#999999",
+  beta.color = "#009E73",
+  both.color = "#E69F00",
   hist.y.origin = 1.2,
   hist.height = 1.8,
   margins = c(160.0, 5.5, 5.5, 5.5),
@@ -57,12 +62,15 @@ phiPlot(
 
 alphaBetaPlot(
   gene.outliers,
-  both.outlier.tests = FALSE,
-  alpha.color = "#33a02c",
-  beta.color = "#ff7f00",
-  neutral.color = "gray60",
+  both.outlier.tests = TRUE,
+  alpha.color = "#999999",
+  beta.color = "#009E73",
+  neutral.color = "#E5E5E5",
+  both.color = "#E69F00",
   saveToFile = prefix,
   plotDIR = "./plots",
   showPLOTS = TRUE,
   padding = 0.5,
 )
+
+save.image(file = "clinehelpr_lowerCI.RData")
